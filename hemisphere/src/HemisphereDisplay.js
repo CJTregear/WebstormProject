18399ada3654003a17304fcf01e42d3c77920fa5
HSKGecko <119751346+HSKGecko@users.noreply.github.com>
2023-06-25 01:46:43 +0100

    Adding Images to Component

--- hemisphere/src/HemisphereDisplay.js
+++ hemisphere/src/HemisphereDisplay.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import northernHem from './images/northern_hemisphere.png'
+import southernHem from './images/souther_hemisphere.png'
+
 
 const HemisphereDisplay = ({ latitude }) =>{
     console.log(latitude)
     const hemisphere = latitude > 0 ? 'Northern' : 'Southern';
+    const picture = latitude > 0 ? northernHem : southernHem;
+
     return(
     <div>
+        <img src={ picture } alt=""/>
         Hey, you are in the `{hemisphere}` Hemisphere!
     </div>
     )
