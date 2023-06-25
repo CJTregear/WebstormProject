65dcca06c79f399a7bf8482f70b578ab338c6640
HSKGecko <119751346+HSKGecko@users.noreply.github.com>
2023-06-26 00:12:18 +0100

    chapter update

--- hemisphere/src/HemisphereDisplay.js
+++ hemisphere/src/HemisphereDisplay.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import './hemisphere.css';
 import northernHem from './images/northern_hemisphere.png'
 import southernHem from './images/souther_hemisphere.png'
 
+const hemisphereConfig = {
+    Northern: {
+        text: 'It is northern hemisphere',
+        picture: northernHem
+    },
+    Southern: {
+        text: 'It is southern hemisphere',
+        picture: southernHem
+    }
+}
 
 const HemisphereDisplay = ({ latitude }) =>{
     console.log(latitude)
     const hemisphere = latitude > 0 ? 'Northern' : 'Southern';
-    const picture = latitude > 0 ? northernHem : southernHem;
+    // const picture = latitude > 0 ? northernHem : southernHem;
+
+    const {text, picture} = hemisphereConfig[hemisphere]
 
     return(
-    <div>
+        <div className={hemisphere}>
         <img src={ picture } alt=""/>
         Hey, you are in the `{hemisphere}` Hemisphere!
     </div>
