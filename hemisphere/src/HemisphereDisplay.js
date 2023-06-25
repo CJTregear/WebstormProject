3c60a37418cc139bf0667c2127538d56de6d65d4
HSKGecko <119751346+HSKGecko@users.noreply.github.com>
2023-06-25 01:33:00 +0100

    Adding ternary operator

--- hemisphere/src/HemisphereDisplay.js
+++ hemisphere/src/HemisphereDisplay.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const HemisphereDisplay = (props) =>{
-    console.log(props.latitude)
+const HemisphereDisplay = ({ latitude }) =>{
+    console.log(latitude)
+    const hemisphere = latitude > 0 ? 'Northern' : 'Southern';
     return(
     <div>
-        Hey, you are in the Northern Hemisphere!
+        Hey, you are in the `{hemisphere}` Hemisphere!
     </div>
     )
 }
